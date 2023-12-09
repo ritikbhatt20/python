485ed8210b7d5f84a55eae944015990ef8936856
ritik <ritikbhatt020@gmail.com>
2023-12-09 11:14:11 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+score = 10
+lol = 5.66
+multiple_lines = """
+I
+am 
+lol"""
+print(multiple_lines)
+
+x, y = 1, 2
+x = y = 2
---
+++ variables2.py
@@ -0,0 +1,6 @@
+score = 2000
+score = 5.66
+print(score)
+print(type(score))
+
+print(f"Score is {score}")
